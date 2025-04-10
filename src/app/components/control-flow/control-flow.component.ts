9285a2f58c224afe756a781a6cbac1ece5137aea
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-09 21:54:09 -0300

    first commit

---
+++ src/app/components/control-flow/control-flow.component.ts
@@ -0,0 +1,22 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-control-flow',
+  imports: [CommonModule],
+  templateUrl: './control-flow.component.html',
+  styleUrl: './control-flow.component.css'
+})
+export class ControlFlowComponent {
+   mostrar : boolean = true;
+   color : string = "green"
+   cores = ["azul", "amarelo", "vermelho"]
+   itemSelecionado:string = 'um';
+   trocaValor(event:Event) : void{
+    const elementoSelecionado = event.target as HTMLSelectElement
+    this.itemSelecionado = elementoSelecionado.value
+   }
+   constructor(){
+
+   }
+}
---
+++ src/app/components/interpolacao/interpolacao.component.ts
@@ -0,0 +1,21 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-interpolacao',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './interpolacao.component.html',
+  styleUrl: './interpolacao.component.css'
+})
+
+export class InterpolacaoComponent {
+  name: string = 'Caio';
+  age: number = 20;
+  job = 'Developer';
+  hobbies = ['Music', 'Sports', 'Movies'];
+  car = { make: 'Mercedes', model: 'AMG' };
+  //para exibir uma imagem busque uma imagem na web e salve dentro da pasta assets
+  imageUrl = 'assets/cara.png';
+
+}
